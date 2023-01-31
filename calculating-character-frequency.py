5ed899d9f134389fdb82450704f54ea543c9e586
Sophia Filipovic <103005707+sophiafil@users.noreply.github.com>
2023-01-31 11:19:34 -0500

    Create calculating-character-frequency.py

---
+++ calculating-character-frequency.py
@@ -0,0 +1,24 @@
+def charCount(userInput):
+    # Makes user input all lowercase
+    fixedInput =  userInput.lower()
+    dict = {}
+    for i in fixedInput:
+        key = dict.keys()
+        # Adds frequencies for each character
+        if i in key:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    # Prints the number of characters seen in the text (not the length of the text)
+    print("Total number of characters including punctuation characters: ", len(key))
+    return dict
+
+# Asks user the enter in text
+print("Please enter in any ammount of text: ")
+userInput = input()
+
+# Gets rid of whitespace
+newUserInput = (userInput.replace(" ", ""))
+
+# Prints the list of the characters and the number of the time it shows in the text
+print(charCount(newUserInput))
